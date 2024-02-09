96b638066cd3c35c927e9415ec9ce0a5556a04e7
AFK-Shark <graffin.may@yandex.ru>
2024-02-09 11:19:07 +0300

    Shop (Click)

--- Assets/Code/EnemyClick.cs
+++ Assets/Code/EnemyClick.cs
@@ -5,16 +5,38 @@ using UnityEngine.UI;
 
 public class EnemyClick : MonoBehaviour
 {
-    [SerializeField] int soul;
-    public Text soulPoints;
+    [SerializeField] int Score;
+    public int[] CostInt;
+    private int ClickScore = 1;
+
+    public GameObject ShopPanel;
+
+    public Text[] CostText;
+    public Text ScoreText;
 
     public void ButtonClick()
     {
-        soul++;
+        Score += ClickScore;
     }
 
     void Update()
     {
-        soulPoints.text = soul.ToString();
+        ScoreText.text = Score.ToString();
+    }
+
+    public void ShowAndHideShopPanel()
+    {
+        ShopPanel.SetActive(!ShopPanel.activeSelf);
+    }
+
+    public void OnClickBuyLevel()
+    {
+        if (Score >= CostInt[0])
+        {
+            Score -= CostInt[0];
+            CostInt[0] *= 2;
+            ClickScore *= 2;
+            CostText[0].text = CostInt[0] + "";
+        }
     }
 }
