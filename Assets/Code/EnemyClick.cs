866ab75be04c50a48ab19f00854b685554f5efe1
AFK-Shark <graffin.may@yandex.ru>
2024-05-28 17:13:15 +0300

    Visual + Store (part 3)
    - UI (All)
    - Shop (Not All)
    - - Auto Click
    - - Level Click

--- Assets/Code/EnemyClick.cs
+++ Assets/Code/EnemyClick.cs
@@ -9,9 +9,11 @@ public class EnemyClick : MonoBehaviour
     public int[] CostInt;
     private int ClickScore = 1;
 
-    public GameObject ShopPanel;
+    public int[] CostIntAUTO;
+    private int ClickScoreAUTO = 1;
 
     public Text[] CostText;
+    public Text[] CostTextAUTO;
     public Text ScoreText;
 
     public void ButtonClick()
@@ -19,15 +21,15 @@ public class EnemyClick : MonoBehaviour
         Score += ClickScore;
     }
 
+    //Обновление Счета
+
     void Update()
     {
         ScoreText.text = Score.ToString();
+
     }
 
-    public void ShowAndHideShopPanel()
-    {
-        ShopPanel.SetActive(!ShopPanel.activeSelf);
-    }
+    //Покупка и обновление статуса магазина
 
     public void OnClickBuyLevel()
     {
@@ -35,8 +37,37 @@ public class EnemyClick : MonoBehaviour
         {
             Score -= CostInt[0];
             CostInt[0] *= 2;
-            ClickScore *= 2;
-            CostText[0].text = CostInt[0] + "";
+            ClickScore += 2;
+            CostText[0].text = CostInt[0] + "$";
         }
     }
+
+    public void ByAutoClick() 
+    {
+        if (Score >= CostIntAUTO[0])
+        {
+            Score -= CostIntAUTO[0];
+            StartCoroutine(AutoClick());
+            ClickScoreAUTO += 1;
+            CostIntAUTO[0] *= 2;
+            CostTextAUTO[0].text = CostIntAUTO[0] + "$";
+        }
+
+        //if (ClickScoreAUTO == 4)
+        //{
+            
+        //}
+    }
+
+    //+1 в секунду
+
+    IEnumerator AutoClick()
+    {
+        while (true)
+        {
+            Score += 1;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
 }
