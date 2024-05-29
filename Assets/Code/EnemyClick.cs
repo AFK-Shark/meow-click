b1b62d5395d5da1433612b8de305aadd21517339
AFK-Shark <graffin.may@yandex.ru>
2024-05-29 17:44:52 +0300

    Additions
    - Visual (Part 4)
    
    - - Unit
    - - Button meow
    - - Slider
    
    - Music
    - Progression
    - Achivments

--- Assets/Code/EnemyClick.cs
+++ Assets/Code/EnemyClick.cs
@@ -5,7 +5,25 @@ using UnityEngine.UI;
 
 public class EnemyClick : MonoBehaviour
 {
-    [SerializeField] int Score;
+    [SerializeField] int Score; //Счет на данный момент
+    private int total_score; //Общий счет для достижений
+
+    //Объекты для достижений
+
+    [SerializeField] GameObject Achivment;
+    [SerializeField] GameObject DefoltAchivment;
+
+    [SerializeField] GameObject Achivment_1;
+    [SerializeField] GameObject DefoltAchivment_1;
+
+    [SerializeField] GameObject Achivment_2;
+    [SerializeField] GameObject DefoltAchivment_2;
+
+    [SerializeField] GameObject Achivment_3;
+    [SerializeField] GameObject DefoltAchivment_3;
+
+    //Счетчики и текст
+
     public int[] CostInt;
     private int ClickScore = 1;
 
@@ -16,16 +34,62 @@ public class EnemyClick : MonoBehaviour
     public Text[] CostTextAUTO;
     public Text ScoreText;
 
+    //Добавление изображений юнитов
+
+    public GameObject Unit1_Image;
+    public GameObject Unit2_Image;
+
     public void ButtonClick()
     {
         Score += ClickScore;
+        total_score++;
     }
 
     //Обновление Счета
 
     void Update()
     {
+        //Обновление текста
+
         ScoreText.text = Score.ToString();
+
+        //Проверка условия появления юнита
+
+        if (ClickScoreAUTO == 5)
+        {
+            Unit1_Image.SetActive(true);
+        }
+
+        if (ClickScoreAUTO == 10)
+        {
+            Unit2_Image.SetActive(true);
+        }
+
+        //Проверка условий для достижений
+
+        if (Score == 1)
+        {
+            Achivment.SetActive(true);
+            DefoltAchivment.SetActive(false);
+        }
+
+        if (Score == 100)
+        {
+            Achivment_1.SetActive(true);
+            DefoltAchivment_1.SetActive(false);
+        }
+
+        if (Score == 10000)
+        {
+            Achivment_2.SetActive(true);
+            DefoltAchivment_2.SetActive(false);
+        }
+
+        if (Score == 100000)
+        {
+            Achivment_3.SetActive(true);
+            DefoltAchivment_3.SetActive(false);
+        }
 
     }
 
@@ -36,7 +100,7 @@ public class EnemyClick : MonoBehaviour
         if (Score >= CostInt[0])
         {
             Score -= CostInt[0];
-            CostInt[0] *= 2;
+            CostInt[0] += CostInt[0] / 2;
             ClickScore += 2;
             CostText[0].text = CostInt[0] + "$";
         }
@@ -49,14 +113,9 @@ public class EnemyClick : MonoBehaviour
             Score -= CostIntAUTO[0];
             StartCoroutine(AutoClick());
             ClickScoreAUTO += 1;
-            CostIntAUTO[0] *= 2;
+            CostIntAUTO[0] += CostIntAUTO[0] / 2;
             CostTextAUTO[0].text = CostIntAUTO[0] + "$";
         }
-
-        //if (ClickScoreAUTO == 4)
-        //{
-            
-        //}
     }
 
     //+1 в секунду
